aa6a88e1d3df33bbba150bb0673733416e21d96a
mariammomo <mmo6385@yahoo.com>
2020-02-23 08:59:32 +0200

    ok

--- tourbi/tourbi/js/telego.js
+++ tourbi/tourbi/js/telego.js
@@ -36,12 +36,6 @@ function makeRowEditable(tr){
 	$( this ).val(dic[ index ]) ;
 });	
 
-$(".dropdown-menu li a").click(function(){
-
-	$(this).parents(".btn-group").find('.selection').text($(this).text());
-	$(this).parents(".btn-group").find('.selection').val($(this).text());
-  
-  });
 
 }
 
